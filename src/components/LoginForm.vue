9a8b364414a99ad97e3dfae17f5f7098acbbd93b
bagebear <bagebear@gmail.com>
2021-12-02 02:41:43 +0800

    feat: update login page and form(front-end-check)

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,84 +1,122 @@
 <template>
-  <form>
-    <div class="form-label-group mb-2">
-      <label for="email">email</label>
-      <input
-        id="email"
-        v-model="email"
-        name="email"
-        type="email"
-        class="form-control"
-        placeholder=""
-        autocomplete="username"
-        required
-        autofocus
-      />
+  <form @submit.stop.prevent="handleSubmit">
+    <div class="login-form__input-box form__input-box">
+      <label class="form__input-box__label" for="email">email</label>
+      <div
+        class="
+          login-form__input-box__input-container
+          form__input-box__input-container
+        "
+      >
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="
+            login-form__input-box__input-container__input
+            form__input-box__input-container__input
+          "
+          required
+          autofocus
+        />
+      </div>
     </div>
 
-    <div class="form-label-group mb-3">
-      <label for="password">Password</label>
-      <input
-        id="password"
-        v-model="password"
-        name="password"
-        type="password"
-        class="form-control"
-        placeholder=""
-        autocomplete="current-password"
-        required
-      />
+    <div class="login-form__input-box form__input-box">
+      <label class="form__input-box__label" for="password">密碼</label>
+      <div
+        class="
+          login-form__input-box__input-container
+          form__input-box__input-container
+        "
+      >
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="
+            login-form__input-box__input-container__input
+            form__input-box__input-container__input
+          "
+          required
+        />
+      </div>
     </div>
 
-    <div class="btn login__btn">登入</div>
+    <button class="login-form__btn btn">
+      登入
+    </button>
 
-    <div class="button--small">
-      <router-link to="/signup" class="button__text--small">註冊Alphitter</router-link>
-      <span> · </span>
-      <span class="button__text--small"> 後台登入</span>
+    <div 
+      class="
+        login-form__btn--small
+        btn--small
+    ">
+      <router-link v-if="!adminToggled" to="/signup" class="btn--small__text">
+        註冊Alphitter
+      </router-link>
+      <span v-if="!adminToggled"> · </span>
+      <span class="btn--small__text" @click="toggleAdmin">
+        {{ adminToggled ? "前台登入" : "後台登入" }}
+      </span>
     </div>
   </form>
 </template>
 
 <script>
+import "./../assets/scss/components/_form.scss";
+import "./../assets/scss/components/_button.scss";
+
 export default {
-   data() {
+  name: 'LoginForm',
+  data() {
     return {
       email: "",
       password: "",
       isProcessing: false,
+      adminToggled: false,
     };
+  },
+
+  methods: {
+    toggleAdmin() {
+      console.log("toggleAdmin");
+      this.adminToggled = !this.adminToggled;
+    },
+
+    async handleSubmit() {
+      try {        
+        if(!this.email) {
+          throw new Error('請輸入email')
+        } 
+
+        if(!this.password) {
+          throw new Error('請輸入密碼')
+        } 
+
+        this.$router.push("/home");
+
+      } catch (error) { 
+        console.log('error')
+      }      
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.form-label-group {
-  position: relative;
-  width: 540px;
-  height: 52px;
-  margin-top: 30px;
-  border: none;
-  border-radius: 0px;
-  label {
-    position: absolute;
-    color: $clr-form-secondary;
-    top: 5px;
-    left: 10px;
+.login-form {
+  &__btn {
+    width: 540px;
+    height: 46px;
+    margin-top: 40px;
+
+    &--small {
+      margin-top: 20px;
+    }
   }
-}
-
-.form-control {
-  border: none;
-  background: $clr-form-bg;
-  border-bottom: 2px solid $clr-form-secondary;
-  border-radius: 0px 0px 4px 4px;
-  width: 100%;
-  height: 100%;
-}
-
-.login__btn {
-  width: 540px;
-  height: 46px;
 }
 
 </style>
