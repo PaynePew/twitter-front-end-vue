7fbaaa2d370352362a1da00e82dd74d73522f45e
bagebear <bagebear@gmail.com>
2021-12-02 15:32:53 +0800

    feat: admin user page create

--- src/components/AdminUserCard.vue
+++ src/components/AdminUserCard.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>AdminUserCard</h1>
+  <table class="admin-user-card__table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Category</th>
+        <th scope="col">Name</th>
+        <th scope="col" width="300">操作</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="restaurant in restaurants" :key="restaurant.id">
+        <th scope="row">
+          {{ restaurant.id }}
+        </th>
+        <td>{{ restaurant.Category ? restaurant.Category.name : "未分類" }}</td>
+        <td>{{ restaurant.name }}</td>
+        <td class="d-flex justify-content-between">
+          <router-link
+            :to="{name: 'admin-restaurant', params: {id: restaurant.id}}"
+             class="btn btn-link"
+            >
+            Show
+          </router-link>
+
+          <router-link
+             :to="{name: 'admin-restaurant-edit', params: {id: restaurant.id}}"
+             class="btn btn-link"
+          >
+            Edit
+          </router-link>
+
+          <button 
+            type="button" 
+            class="btn btn-link"
+            @click.stop.prevent="deleteRestaurant(restaurant.id)"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
--- src/views/AdminUsers.vue
+++ src/views/AdminUsers.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>AdminUser</h1>
+  <div class="container">
+    <AdminUserCard/>
+  </div>
 </template>
+
+<script>
+import AdminUserCard from './../components/AdminUserCard.vue'
+
+export default {
+  name: 'AdminUsers',
+  components: {
+    AdminUserCard
+  }
+}
+</script>
+
+
