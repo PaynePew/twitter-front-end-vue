e522aee3be50bae2e70574f97e750ea818ac8320
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-02 15:25:17 +0800

    feat: add condition render

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Modal</h1>
+  <section class="modal">
+    <div class="modal__mask">
+      <div class="modal__wrapper">
+        <div class="modal__container">
+          <div class="modal__header"></div>
+          <div class="modal__body">
+            <form class="article-create__form" action="">
+              <input
+                class="article-create__input"
+                type="textarea"
+                placeholder="有甚麼新鮮事?"
+              />
+              <div class="article-create__footer">
+                <button class="article-create__btn btn">推文</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
