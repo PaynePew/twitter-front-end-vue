be3db3104c1ac1c45558fdf69b2a620de08f904f
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-03 10:46:10 +0800

    style: add UserInfoTab

--- src/components/UserInfoTab.vue
+++ src/components/UserInfoTab.vue
@@ -1,3 +1,66 @@
 <template>
-  <h1>UserInfoTab</h1>
+  <nav role="navigation" aria-label="個人資料時間軸" class="user-tab">
+    <div class="user-tab__wrapper">
+      <div class="user-tab__container">
+        <div role="tablist" class="user-tab__tablist">
+          <div class="user-tab__tab">
+            <router-link class="user-tab__nav" :to="{ name: 'UserInfo' }"
+              >推文</router-link
+            >
+          </div>
+          <div class="user-tab__tab">
+            <router-link
+              class="user-tab__nav"
+              :to="{ name: 'UserInfoWithReplies' }"
+              >推文與回覆</router-link
+            >
+          </div>
+          <div class="user-tab__tab">
+            <router-link class="user-tab__nav" :to="{ name: 'UserInfoLikes' }"
+              >喜歡的內容</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.user-tab {
+  &__wrapper {
+    border-bottom: 1px solid $clr-border;
+  }
+  &__tablist {
+    display: flex;
+  }
+  &__tab {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 130px;
+    height: 52px;
+    color: $clr-secondary;
+  }
+  &__nav {
+    font-weight: bold;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    flex: 1 0 auto;
+    &:visited {
+      color: inherit;
+    }
+  }
+  .exact-active {
+    color: $clr-third;
+    border-bottom: 2px solid $clr-third;
+  }
+}
+</style>
--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>UserInfo</h1>
+  <UserInfoCard />
+  <UserInfoTab />
+  <router-view :key="$route.fullPath" />
+  <ArticleCard :articles="articles" />
 </template>
+
+<script>
+import UserInfoCard from "@/components/UserInfoCard.vue";
+import UserInfoTab from "@/components/UserInfoTab.vue";
+import ArticleCard from "@/components/ArticleCard.vue";
+import { usersDummy } from "@/store/dummy/usersDummy";
+import { articlesDummy } from "@/store/dummy/articlesDummy";
+
+export default {
+  components: {
+    ArticleCard,
+    UserInfoCard,
+    UserInfoTab,
+  },
+  data() {
+    return {
+      currentUser: [],
+      articles: [],
+    };
+  },
+  beforeMount() {
+    this.currentUser = usersDummy[0];
+    this.articles = articlesDummy;
+  },
+  // beforeRouteUpdate(to, from, next) {
+  //   console.log("from", from.params.userName);
+  //   next();
+  // },
+};
+</script>
+
+<style lang="scss" scoped></style>
