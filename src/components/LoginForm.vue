1dc72f45c6157b4c5acb81fba06d9bab0be8abf4
bagebear <bagebear@gmail.com>
2021-12-05 01:08:56 +0800

    feat: update warning message style

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="form__warning-box" v-if="warning">
-    <span class="form__warning-message">{{ warning }}</span>
+  <div class="form__notice-box" v-if="notice">
+    <span class="form__notice-message">{{ notice }}</span>
     <img
-      class="form__warning-close"
+      class="form__notice-close"
       src="./../assets/img/icon_close_black@2x.png"
-      @click="warning = ''"
+      @click="notice = ''"
       alt=""
     />
   </div>
@@ -12,9 +12,9 @@
     type="button" 
     class="btn btn-primary" 
     id="liveAlertBtn"
-    @click="alert('You found me','success')"
+    @click="toggleNotice('You found me','success')"
   >
-    Warning Test
+    Notice Test
   </button> -->
   <form @submit.stop.prevent="handleSubmit">
     <div class="login-form__input-box form__input-box">
@@ -69,7 +69,7 @@ export default {
       password: "",
       isProcessing: false,
       adminToggled: false,
-      warning: "",
+      notice: "",
     };
   },
 
@@ -99,8 +99,8 @@ export default {
       }
     },
 
-    alert(message) {
-      this.warning = message;
+    toggleNotice(message) {
+      this.notice = message;
     },
   },
 };
