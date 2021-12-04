135a63099b902fcf42edf788acca056a4f17c19d
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-04 14:46:59 +0800

    feat: add ArticleCardShow page and page click event

--- src/views/ArticleShow.vue
+++ src/views/ArticleShow.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>ArticleShow</h1>
+  <AppBar :status="'推文'" :stepper="true" />
+  <ArticleCardShow :article="currentArticle" />
+  <ArticleCardWithReply :articles="articles" />
 </template>
+
+<script>
+import AppBar from "@/components/AppBar.vue";
+import ArticleCardShow from "@/components/ArticleCardShow.vue";
+import ArticleCardWithReply from "@/components/ArticleCardWithReply.vue";
+import { articlesDummy } from "@/store/dummy/articlesDummy";
+
+export default {
+  components: {
+    ArticleCardWithReply,
+    ArticleCardShow,
+    AppBar,
+  },
+  data() {
+    return {
+      currentUser: [],
+      currentArticle: [],
+      articles: [],
+    };
+  },
+  beforeMount() {
+    const { articleId } = this.$route.params;
+    console.log(articleId);
+    this.currentArticle = articlesDummy[0];
+    this.articles = articlesDummy;
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
