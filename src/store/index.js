d3c6785308734226a368fd9bca524e951dd35598
bagebear <bagebear@gmail.com>
2021-12-06 00:52:26 +0800

    chore: add currentUser status and SetCurrentUser mutations (vuex)

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    currentUser: {
+      id: "",
+      account: "",
+      email: "",
+      name: "",
+      role: "",
+    },
+    isAuthenticated: false,
+  },
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser,
+      };
+      state.isAuthenticated = true;
+      state.token = localStorage.getItem('token');
+    },    
+  },
   actions: {},
   modules: {},
 });
