29e6b7713381bda07894d2d3856d3dca303201b8
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-05 11:30:46 +0800

    chore: reset sass and node-sass

--- src/views/UserSetting.vue
+++ src/views/UserSetting.vue
@@ -1,21 +1,21 @@
 <template>
-    <AccountForm :initialUser="currentUser"/>
+  <AccountForm :initialUser="currentUser" />
 </template>
 
 <script>
 import AccountForm from "./../components/AccountForm.vue";
 
 const dummyUser = {
-    id: 3,
-    account: "tz1VJJNhqL3irfvxKcJtuEn3Ti97hNUWQWcd",
-    email: "fklasing2@china.com.cn",
-    password: "He1xnjP",
-    name: "Florri Klasing",
-    introduction: "96.72.192.4",
-    avatar: "https://robohash.org/sedvitaeeveniet.png?size=50x50&set=set1",
-    cover: "https://robohash.org/autimpeditillo.png?size=50x50&set=set1",
-    role: "",
-  }
+  id: 3,
+  account: "tz1VJJNhqL3irfvxKcJtuEn3Ti97hNUWQWcd",
+  email: "fklasing2@china.com.cn",
+  password: "He1xnjP",
+  name: "Florri Klasing",
+  introduction: "96.72.192.4",
+  avatar: "https://robohash.org/sedvitaeeveniet.png?size=50x50&set=set1",
+  cover: "https://robohash.org/autimpeditillo.png?size=50x50&set=set1",
+  role: "",
+};
 
 export default {
   name: "Login",
@@ -26,28 +26,26 @@ export default {
     return {
       currentUser: {
         id: -1,
-        account: '',
-        email: '',
-        password: '',
-        name: '',
-        introduction: '',
-        avatar: '',
-        cover: '',
-        role: ''
-      }
-    }
+        account: "",
+        email: "",
+        password: "",
+        name: "",
+        introduction: "",
+        avatar: "",
+        cover: "",
+        role: "",
+      },
+    };
   },
   mounted() {
     this.fetchUser();
   },
   methods: {
     fetchUser() {
-      this.currentUser = dummyUser
-    }
-  }
+      this.currentUser = dummyUser;
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
