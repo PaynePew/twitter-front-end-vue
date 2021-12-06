3b15c80e0ab43e302b8c6d18fd0613634f33ac63
bagebear <bagebear@gmail.com>
2021-12-06 17:19:16 +0800

    chore: authorization api and data key update

--- src/apis/authorization.js
+++ src/apis/authorization.js
@@ -4,18 +4,18 @@ export default {
   //TODO 登入資料api待修正 account,password
   signIn({ email, password }) {
     console.log('email: ',email,' password: ', password);
-    return apiHelper.post('/signin', {
+    return apiHelper.post('/users/signin', {
       email,
       password
     })
   },
-  signUp({ name, account, email, password, passwordCheck }) {
-    return apiHelper.post('/signup', {
+  signUp({ name, account, email, password, checkPassword }) {
+    return apiHelper.post('/users', {
       name,
       account,
       email,
       password,
-      passwordCheck
+      checkPassword
     })
   }
 }
