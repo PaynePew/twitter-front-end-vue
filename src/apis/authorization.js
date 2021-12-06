ee10e32e1f5cdc423dbf32d0e665196fad1f8745
bagebear <bagebear@gmail.com>
2021-12-07 02:58:27 +0800

    feat: use account to replace email when login

--- src/apis/authorization.js
+++ src/apis/authorization.js
@@ -2,10 +2,10 @@ import { apiHelper } from "../utils/helpers";
 
 export default {
   //TODO 登入資料api待修正 account,password
-  signIn({ email, password }) {
-    console.log("email: ", email, " password: ", password);
+  signIn({ account, password }) {
+    console.log("account: ", account, " password: ", password);
     return apiHelper.post("/users/signin", {
-      email,
+      account,
       password,
     });
   },
