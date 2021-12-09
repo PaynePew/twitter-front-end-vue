7e8ad2acbf00a9c2c7edf9991addcc7a758719ac
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-09 15:22:01 +0800

    feat: add ArticleShow with reply in API

--- src/views/ArticleShow.vue
+++ src/views/ArticleShow.vue
@@ -1,8 +1,10 @@
 <template>
   <AppBar :status="'推文'" :stepper="true" />
   <Spinner v-if="isLoading" />
-  <ArticleCardShow v-else :current-article="currentArticle" />
-  <ArticleCardWithReply :articles="articles" />
+  <div v-else>
+    <ArticleCardShow :current-article="currentArticle" />
+    <ArticleCardWithReply :allReplies="allReplies" :owner="owner" />
+  </div>
 </template>
 
 <script>
@@ -10,7 +12,6 @@ import AppBar from "@/components/AppBar.vue";
 import ArticleCardShow from "@/components/ArticleCardShow.vue";
 import ArticleCardWithReply from "@/components/ArticleCardWithReply.vue";
 import Spinner from "@/components/Spinner.vue";
-import { articlesDummy } from "@/store/dummy/articlesDummy";
 import articlesAPI from "@/apis/articles";
 
 export default {
@@ -24,25 +25,35 @@ export default {
     return {
       currentUser: [],
       currentArticle: {},
-      articles: [],
+      allReplies: [],
+      owner: [],
       isLoading: true,
     };
   },
   created() {
     const { articleId } = this.$route.params;
-    this.currentArticle = articlesDummy[0];
-    this.articles = articlesDummy;
     this.getArticleShow(articleId);
   },
   methods: {
     async getArticleShow(articleId) {
       try {
+        this.isLoading = true;
         const { data } = await articlesAPI.getArticleShow(articleId);
-        console.log("articleshow", data);
         this.currentArticle = data;
+        await this.getArticleReply(articleId);
         this.isLoading = false;
       } catch (error) {
         this.isLoading = false;
+        console.log(error);
+      }
+    },
+    async getArticleReply(articleId) {
+      try {
+        const { data } = await articlesAPI.reply.get(articleId);
+        console.log(data);
+        this.allReplies = data.RepliedUsers;
+        this.owner = data.User;
+      } catch (error) {
         console.log(error);
       }
     },
