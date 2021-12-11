da42ae120aa1e3e1b5d0b6baf3af0169689d5df4
bagebear <bagebear@gmail.com>
2021-12-11 14:01:07 +0800

    feat: create ChatPrivate.vue

---
+++ src/views/ChatPrivate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="private-message__container">
+    <ChatUserList />
+    <Chatroom />
+  </div>
+</template>
+
+<script>
+import ChatUserList from "../components/ChatUserList.vue";
+import Chatroom from "../components/Chatroom.vue";
+
+export default {
+  components: {
+    ChatUserList,
+    Chatroom,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.private-message {
+  &__container {
+    width: 1063px;
+    height: 100%;
+    display: flex;
+  }
+}
+</style>
