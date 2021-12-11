97f5c4318ea631f8de920782299b03b5856f6a4f
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-12-11 21:41:37 +0800

    feat: add socket helper for further use

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import io from "socket.io-client";
 
 // const baseURL = "https://ac3-twitter-api.herokuapp.com/api";
-const baseURL = "https://ac3-twitter-api-jen.herokuapp.com/api";
-// const baseURL = "http://localhost:3000/api";
+// const baseURL = "https://ac3-twitter-api-jen.herokuapp.com/api";
+const baseURL = "http://localhost:3000/api";
+const socketURL = "http://localhost:3000";
 
 const axiosInstance = axios.create({
   baseURL,
@@ -22,3 +24,5 @@ axiosInstance.interceptors.request.use(
 );
 
 export const apiHelper = axiosInstance;
+
+export const socket = io(socketURL);
